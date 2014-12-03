6ca4e6284617e06712ddbce8b589dcea77292ddb
reaper7md <reaper7md@gmail.com>
2014-12-03 22:08:25 +0100

    first commit

---
+++ i2c_sht21.c
@@ -0,0 +1,89 @@
+/* reaper7 */
+
+#include "driver/i2c.h"
+#include "driver/i2c_sht21.h"
+#include "osapi.h"
+
+void ICACHE_FLASH_ATTR
+SHT21_SoftReset()
+{
+  //Soft reset the SHT21
+  i2c_start();
+  i2c_writeByte(SHT21_ADDRESS);
+  i2c_check_ack();
+  i2c_writeByte(SOFT_RESET);
+  i2c_check_ack();
+  i2c_stop();
+}
+
+void ICACHE_FLASH_ATTR
+SHT21_Init()
+{
+  SHT21_SoftReset();
+}
+
+int ICACHE_FLASH_ATTR
+SHT21_GetVal(uint8 val)
+{
+  i2c_start(); //Start i2c
+  i2c_writeByte(SHT21_ADDRESS);
+  if (!i2c_check_ack()) {
+    //os_printf("-%s-%s slave not ack... return \r\n", __FILE__, __func__);
+    i2c_stop();
+    return(0);
+  }
+
+  if (val==0)
+    i2c_writeByte(TRIGGER_TEMP_MEASURE_NOHOLD);
+  else
+    i2c_writeByte(TRIGGER_HUMD_MEASURE_NOHOLD);
+  
+  if (!i2c_check_ack()) {
+    //os_printf("-%s-%s slave not ack... return \r\n", __FILE__, __func__);
+    i2c_stop();
+    return(0);
+  }
+  
+  os_delay_us(20);
+  
+  i2c_stop();
+  
+  os_delay_us(70000);
+  
+  uint8 ack = 0;
+  while (!ack) {
+    i2c_start();
+    i2c_writeByte(SHT21_ADDRESS+1);
+    ack = i2c_check_ack();
+    if (!ack) i2c_stop();
+  }
+  //os_printf("-%s-%s get ack \r\n", __FILE__, __func__);
+
+  uint8 msb = i2c_readByte();
+  i2c_send_ack(1);
+  uint8 lsb = i2c_readByte();
+  i2c_send_ack(0);
+  i2c_stop();
+     
+  uint16_t _rv = msb << 8;
+  _rv += lsb;
+  _rv &= ~0x0003; 
+ 
+  //os_printf("-%s-%s RAW:%d \r\n", __FILE__, __func__,_rv);
+
+  float rv = _rv;
+  
+  if (val==0) {
+    rv *= 175.72;
+    rv /= 65536;
+    rv -= 46.85;
+    //os_printf("-%s-%s T:%d \r\n", __FILE__, __func__,(int)(rv*10)); 
+  } else {
+    rv *= 125.0;
+    rv /= 65536;
+    rv -= 6.0; 
+    //os_printf("-%s-%s H:%d \r\n", __FILE__, __func__,(int)(rv*10));
+  }
+  
+  return (int)(rv*10);
+}
---
+++ i2c_sht21.h
@@ -0,0 +1,22 @@
+#ifndef __I2C_SHT21_H
+#define	__I2C_SHT21_H
+
+#include "c_types.h"
+#include "ets_sys.h"
+#include "osapi.h"
+
+#define SHT21_ADDRESS 0x80
+
+#define TRIGGER_TEMP_MEASURE_HOLD  0xE3
+#define TRIGGER_HUMD_MEASURE_HOLD  0xE5
+#define TRIGGER_TEMP_MEASURE_NOHOLD  0xF3
+#define TRIGGER_HUMD_MEASURE_NOHOLD  0xF5
+#define WRITE_USER_REG  0xE6
+#define READ_USER_REG  0xE7
+#define SOFT_RESET  0xFE
+
+void SHT21_Init(void);
+int SHT21_GetVal(uint8 val); //0->temperature; !=0->humidity
+void SHT21_SoftReset(void);
+
+#endif
